a6ef5b7328f7a7d4c305d6b6d20bd1badd5c76b9
manasa <manasatm150@gmail.com>
2024-08-08 09:10:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("java script let const var")
+
+//variable declaration
+var firstName;
+firstName ="manasa"
+console.log("First Name :",firstName)
+
+
+//variable declaration and initialization
+var a = 20;
+
+//data types
+var str = "java script"//string//
+console.log("str : ",str)
+console.log("typeof(str)",typeof(str))
+
+var num = 10; //num//
+console.log("num :",num)
+
+var bool1 = true;//Boolean values//
+console.log("bool1:",bool1)
+
+var bool2 = false;
+console.log("pool2 :",bool2)
+
+var varUndefined = undefined
+console.log ("varundefined : ",)
+
+
+
+
+
+
