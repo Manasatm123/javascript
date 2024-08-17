93ad75ab65f19596d44034b597190f8d1a370582
manasa <manasatm150@gmail.com>
2024-08-17 12:07:14 +0530

    second commit

--- javasript.js
+++ javasript.js
@@ -1,5 +1,7 @@
 //write a function to find the sum of elements of a numerical array
 {
+
+console.log("sum of elements of a numerical array")
 function sumOfArray(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -17,6 +19,7 @@ console.log("\n\n\n\n\n\n")
 
 // write a function to find the average of element of an array  
 {
+console.log("average of element of an array")    
 function averageOfArray(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -36,6 +39,7 @@ console.log("\n\n\n\n\n\n")
 // write a function to find the maximum and minimum element of an array 
 
 {
+    console.log("maximum and mininum element of an array")
     function findMinMax(arr) {
         let min = arr[0];
         let max = arr[0];
@@ -53,6 +57,7 @@ console.log("\n\n\n\n\n\n")
     }
     
     let arr = [10, 20, 30, 40, 50];
+    console.log("array:",arr)
     let result = findMinMax(arr);
     console.log(`Minimum: ${result.min}, Maximum: ${result.max}`);
 }
@@ -62,6 +67,7 @@ console.log("\n\n\n\n\n\n")
 // write a function to convert the negative numbers of an array to positive numbers
 
 {
+    console.log("convert the negative numbers of an array to positive numbers")
 function convertNegativesToPositives(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < 0) {
@@ -83,6 +89,7 @@ console.log("\n\n\n\n\n\n")
 // write a function to find out the sum of odd numbers of an array
 
 {
+    console.log("find out the sum of odd numbers of an array") 
     function sumOfOddNumbers(arr) {
         let sum = 0;
         for (let i = 0; i < arr.length; i++) {
@@ -105,6 +112,7 @@ console.log("\n\n\n\n\n\n")
 // write a function to calculate the sum of even numbers of an array
 
 {
+    console.log("calculate the sum of even numbers of an array")
     function sumOfEvenNumbers(arr) {
         let sum = 0;
         for (let i = 0; i < arr.length; i++) {
@@ -115,7 +123,7 @@ console.log("\n\n\n\n\n\n")
         return sum;
     }
     
-    let arr = [10, 21, 30, 41, 50];
+    let arr = [1, 2, 3, 4, 5];
     console.log("array :",arr)
     let result = sumOfEvenNumbers(arr);
     console.log("sum of even numbers in array : ",result); 
@@ -126,6 +134,7 @@ console.log("\n\n\n\n\n\n")
 
 // write a function to find the factorial of an array using recursion
 {
+console.log(" find the factorial of an array using recursion")
 function factorial(n) {
     if (n === 0 || n === 1) {
         return 1;
